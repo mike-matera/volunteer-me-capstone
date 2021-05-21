5244086e3e36291f2089b315374d76b41f011412
Mike Matera <maximus@fatboycentral.com>
2021-05-20 19:41:07 -0700

    Add initial prisma schema.

---
+++ prisma/seed.js
@@ -0,0 +1,30 @@
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const userData = [
+  {
+    name: 'TestAdmin',
+    email: 'testadmin@test.test',
+  },
+]
+
+async function main() {
+  console.log(`Start seeding ...`)
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    })
+    console.log(`Created user with id: ${user.id}`)
+  }
+  console.log(`Seeding finished.`)
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
