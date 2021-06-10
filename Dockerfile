1a244dc5d4859d2d0f3bf86d160bccc036b78070
Mike Matera <maximus@fatboycentral.com>
2021-06-10 11:22:40 -0700

    For now: squash db on app start.

--- Dockerfile
+++ Dockerfile
@@ -34,6 +34,7 @@ COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/package.json ./package.json
 COPY --from=builder /app/prisma ./prisma
 COPY prod.sh ./prod.sh
+COPY initdb.sh ./initdb.sh
 
 USER nextjs
 
--- prod.sh
+++ prod.sh
@@ -6,8 +6,11 @@
 
 set -e 
 
-while ! npx prisma migrate deploy; do 
-    sleep 10
-done 
+#while ! npx prisma migrate deploy; do 
+#    sleep 10
+#done 
+
+# This is broken for now. Forcibly init the db on container start. BAD!
+. ./initdb.sh
 
 yarn start
