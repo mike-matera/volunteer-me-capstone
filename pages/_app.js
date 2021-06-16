e9f0b9c294466421af4ab50b456b2210de4b622b
Darlene graterol <dcgb93@gmail.com>
2021-06-15 22:35:27 -0700

    website header and smallnavbar changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from "next/head";
+import { Helmet } from 'react-helmet';
 
 function MyApp({ Component, pageProps}) {
   return (
     <>
+      <Helmet>
+        <title>VolunterMe!</title>
+      </Helmet>
       {/* Add the favicon */}
       <Head>
         <link rel="shortcut icon" href="https://static.thenounproject.com/png/3637517-200.png" />
