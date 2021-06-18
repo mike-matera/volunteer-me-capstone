23760d2f54b9e271a43e477e8c1ac1a16a62e5c6
Mike Matera <maximus@fatboycentral.com>
2021-06-17 19:57:33 -0700

    Switch to production migration.

--- prod.sh
+++ prod.sh
@@ -6,11 +6,8 @@
 
 set -e 
 
-#while ! npx prisma migrate deploy; do 
-#    sleep 10
-#done 
-
-# This is broken for now. Forcibly init the db on container start. BAD!
-. ./initdb.sh
+while ! npx prisma migrate deploy; do 
+    sleep 10
+done 
 
 yarn start
